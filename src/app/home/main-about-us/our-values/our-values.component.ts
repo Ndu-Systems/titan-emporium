8026d3851c540eb65ea6a4798a6f286fb6ffee26
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-12-24 12:22:12 +0200

    changes

--- src/app/home/main-about-us/our-values/our-values.component.ts
+++ src/app/home/main-about-us/our-values/our-values.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-values.component.scss']
 })
 export class OurValuesComponent implements OnInit {
-  paragraphs = [`Comming soon`]
+  paragraphs = [`Our values are the core beliefs of our organization. They define our culture and determine the way we engage with our customers and other stake holders, create products and services, and grow our community. 
+  Our values guide us in our everyday decisions and help to set a high standard of work ethics.`,
+`Our team consistently lives by these values and strives to build an exceptional customer service experience`,
+`Through Practical Leadership, Simple, Professional and Democratic practices, we have created a successful company culture 
+that celebrates innovation, encourages agility and individual growth, and inspires success `
+]
   heading ="Our Values"
-  bg="mission-and-vision-png-3.png";
+  bg="vision.png";
 
   constructor() { }
 

fe8f9a7212ed999983c098f30d04581dfb2d98cc
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-02-19 11:27:12 +0200

    Horizontal Directional Drilling

--- src/app/home/main-services/pipe-jacking/pipe-jacking.component.ts
+++ src/app/home/main-services/pipe-jacking/pipe-jacking.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pipe-jacking.component.scss']
 })
 export class PipeJackingComponent implements OnInit {
-  paragraphs=[`Comming soon...`,`Comming soon...`
+  paragraphs=[`Pipe jacking or micro tunnelling is the act of installing large diameter pipelines across 
+  obstacles by tunnelling beneath and installing concrete sleeves or culverts by jacking them or pushing them in place. `,`
+   It is essentially used for installing culverts for canals or stormwater pipes or even for multi-product line sleeves.`
+  ,`Titan Emporium has installed pipelines across major roadways by pipe jacking for major clients like DRD Gold.`
+  ,`Our team are capable of handling any project no matter the size.`
+  
 ];
-  heading ="Comming..."
+  heading ="Pipe Jacking"
   bg="";
   
   constructor() { }
